cde3d53740075dfa3983edcbf18b27715fd1af8c
Thomas Calmant <thomas.calmant@gmail.com>
2023-09-26 17:31:45 +0200

    Added a provider bundle

--- provider/src/main/java/com/kentyou/eclipsecon2023/websocket/provider/SquareWebSocket.java
+++ provider/src/main/java/com/kentyou/eclipsecon2023/websocket/provider/SquareWebSocket.java
@@ -10,39 +10,37 @@
 * Contributors:
 *   Thomas Calmant (Kentyou) - initial implementation
 **********************************************************************/
-package com.kentyou.eclipsecon2023.websocket.backend;
+package com.kentyou.eclipsecon2023.websocket.provider;
 
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Deactivate;
 
 import jakarta.websocket.OnMessage;
-import jakarta.websocket.OnOpen;
 import jakarta.websocket.Session;
 import jakarta.websocket.server.ServerEndpoint;
 
-@ServerEndpoint("/ws/test-annotation")
-@Component(service = WebSocketTestAnnotation.class, immediate = true, property = { "websocket.server=true" })
-public class WebSocketTestAnnotation {
+@ServerEndpoint("/ws/square")
+@Component(service = SquareWebSocket.class, immediate = true, property = { "websocket.server=true" })
+public class SquareWebSocket {
 
     @Activate
     void activate() {
-        System.out.println("WebSocket annotated echo component started");
+        System.out.println("Square WebSocket provider activated");
     }
 
     @Deactivate
     void deactivate() {
-        System.out.println("WebSocket annotated echo component stopped");
-    }
-
-    @OnOpen
-    public void onConnect() {
-        System.out.println("ECHO endpoint connection");
+        System.out.println("Square WebSocket provider deactivated");
     }
 
     @OnMessage
     public void onMessage(String message, Session session) throws Exception {
-        System.out.println("Echoing " + message);
-        session.getBasicRemote().sendText("Echo: " + message);
+        try {
+            long value = Long.parseLong(message);
+            session.getBasicRemote().sendText(String.valueOf(value * value));
+        } catch (NumberFormatException e) {
+            session.getBasicRemote().sendText("Error: " + e);
+        }
     }
 }
